82497f2ddae5f393af5242b76730c9cc8f8e54e6
yyi.mailer@gmail.com <yangyigood108>
2019-02-02 14:23:10 +0800

    dynamically router load component

--- Chapter06/chapter6-full/client/src/router.js
+++ Chapter06/chapter6-full/client/src/router.js
@@ -2,16 +2,26 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import store from './store'
 
-import Login from './components/Login.vue'
-import GeoBlog from './components/GeoBlog.vue'
-import NotFound from './components/NotFound.vue'
+// import Login from './components/Login.vue'
+// import GeoBlog from './components/GeoBlog.vue'
+// import NotFound from './components/NotFound.vue'
 
 Vue.use(VueRouter)
 
+/**
+ * Asynchronously load view (Webpack Lazy loading compatible)
+ * @param  {string}   name     the filename (basename) of the view to load.
+ */
+function view(name) {
+  return function(resolve) {
+    require(['./components/' + name + '.vue'], resolve);
+  }
+};
+
 const routes = [
-  { path: '/', name: 'home', component: GeoBlog, meta: { private: true } },
-  { path: '/login', name: 'login', component: Login },
-  { path: '*', component: NotFound },
+  { path: '/', name: 'home', component:view('GeoBlog') , meta: { private: true } },
+  { path: '/login', name: 'login', component: view('Login') },
+  { path: '*', component: view('NotFound') },
 ]
 
 const router = new VueRouter({
